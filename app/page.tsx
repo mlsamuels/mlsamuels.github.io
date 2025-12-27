60c3672dec019af4224e3f2a9d81a785dc6e5873
Matthew Samuels <mlsamuels@wpi.edu>
2025-12-26 21:34:18 -0600

    Fixed offset elements bug

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,8 @@ export default function Home() {
 
     const dict: {[key: string]: string[]} = {
       java: ["Java is the language I have the most experience in. It's the main language used in my classes and the language I'm most equipped to teach. I have created hundreds of projects in Java and have an intuitive understanding of its syntax. I was the lead Java instructor at SummerTech, where I taught java skills to the other teachers. I was the person who would fix problems no one else could. Because of my experience I can solve most java bugs quickly as I've seen them before.","/javalogo.svg"],
-      python: ["Python is one of my most used languages. I have extensive experience both using and teaching it. I'm also familiar with several libraries and frameworks such as Numpy, Pandas, TensorFlow, Pytorch, and Keras. Python is one of my favorite languages because of its ease of access and concise syntax.", "/pythonlogo.svg"]
+      python: ["Python is one of my most used languages. I have extensive experience both using and teaching it. I'm also familiar with several libraries and frameworks such as Numpy, Pandas, TensorFlow, Pytorch, and Keras. Python is one of my favorite languages because of its ease of access and concise syntax.", "/pythonlogo.svg"],
+      placeholder: ["This is placeholder text!", "globe.svg"],
     }
 
 
@@ -88,7 +89,7 @@ export default function Home() {
         </div>
 
 
-        <div className="circle-container rounded-full bg-zinc-100">
+          <div className="circle-container  rounded-full bg-zinc-100" >
           <div className="center-element flex flex-row  items-center  justify-center gap-4">
             <Image
                 className="dark:invert flex-none "
@@ -99,14 +100,14 @@ export default function Home() {
                 priority
             />
 
-            <p className="flex-1 min-w-0 text-align-justify  text-sm  text-zinc-600 dark:text-zinc-400 max-w-1/2">
+            <p className="flex-1 min-w-0 text-align-justify  text-sm  text-zinc-600 dark:text-zinc-400 max-w-96">
               {aboutText}
             </p>
 
           </div>
 
 
-          <div className="moving-element" style={{'--i': 11}as React.CSSProperties}>
+          <div className="moving-element" style={{'--i': 1}as React.CSSProperties}>
             <Image
                 className="hover:bg-black/[.04]  p-2 rounded-full dark:invert "
                 src="/javalogo.svg"
@@ -118,7 +119,7 @@ export default function Home() {
                 onClick={(e)=>{e.preventDefault(); changeAbout("java");}}
             />
           </div>
-          <div className="moving-element" style={{'--i': 12}as React.CSSProperties }>
+          <div className="moving-element" style={{'--i': 2}as React.CSSProperties }>
             <Image
                 className="dark:invert p-2 hover:bg-black/[.04] rounded-full"
                 src="/pythonlogo.svg"
@@ -130,8 +131,227 @@ export default function Home() {
                 onClick={(e)=>{e.preventDefault(); changeAbout("python");}}
             />
           </div>
+          <div className="moving-element" style={{'--i': 3}as React.CSSProperties }>
+            <Image
+                className="dark:invert p-2 hover:bg-black/[.04] rounded-full"
+                src="/globe.svg"
+                alt="Logo2"
+                width={70}
+                height={70}
+                priority
+                tabIndex={-1}
+                onClick={(e)=>{e.preventDefault(); changeAbout("placeholder");}}
+            />
+          </div>
+          <div className="moving-element" style={{'--i': 4}as React.CSSProperties }>
+            <Image
+                className="dark:invert p-2 hover:bg-black/[.04] rounded-full"
+                src="/globe.svg"
+                alt="Logo2"
+                width={70}
+                height={70}
+                priority
+                tabIndex={-1}
+                onClick={(e)=>{e.preventDefault(); changeAbout("placeholder");}}
+            />
+          </div>
+          <div className="moving-element" style={{'--i': 5}as React.CSSProperties }>
+            <Image
+                className="dark:invert p-2 hover:bg-black/[.04] rounded-full"
+                src="/globe.svg"
+                alt="Logo2"
+                width={70}
+                height={70}
+                priority
+                tabIndex={-1}
+                onClick={(e)=>{e.preventDefault(); changeAbout("placeholder");}}
+            />
+          </div>
+          <div className="moving-element" style={{'--i': 6}as React.CSSProperties }>
+            <Image
+                className="dark:invert p-2 hover:bg-black/[.04] rounded-full"
+                src="/globe.svg"
+                alt="Logo2"
+                width={70}
+                height={70}
+                priority
+                tabIndex={-1}
+                onClick={(e)=>{e.preventDefault(); changeAbout("placeholder");}}
+            />
+          </div>
+          <div className="moving-element" style={{'--i': 7}as React.CSSProperties }>
+            <Image
+                className="dark:invert p-2 hover:bg-black/[.04] rounded-full"
+                src="/globe.svg"
+                alt="Logo2"
+                width={70}
+                height={70}
+                priority
+                tabIndex={-1}
+                onClick={(e)=>{e.preventDefault(); changeAbout("placeholder");}}
+            />
+          </div>
+          <div className="moving-element" style={{'--i': 8}as React.CSSProperties }>
+            <Image
+                className="dark:invert p-2 hover:bg-black/[.04] rounded-full"
+                src="/globe.svg"
+                alt="Logo2"
+                width={70}
+                height={70}
+                priority
+                tabIndex={-1}
+                onClick={(e)=>{e.preventDefault(); changeAbout("placeholder");}}
+            />
+          </div>
+          <div className="moving-element" style={{'--i': 9}as React.CSSProperties }>
+            <Image
+                className="dark:invert p-2 hover:bg-black/[.04] rounded-full"
+                src="/globe.svg"
+                alt="Logo2"
+                width={70}
+                height={70}
+                priority
+                tabIndex={-1}
+                onClick={(e)=>{e.preventDefault(); changeAbout("placeholder");}}
+            />
+          </div>
+          <div className="moving-element" style={{'--i': 10}as React.CSSProperties }>
+            <Image
+                className="dark:invert p-2 hover:bg-black/[.04] rounded-full"
+                src="/globe.svg"
+                alt="Logo2"
+                width={70}
+                height={70}
+                priority
+                tabIndex={-1}
+                onClick={(e)=>{e.preventDefault(); changeAbout("placeholder");}}
+            />
+          </div>
+          <div className="moving-element" style={{'--i': 11}as React.CSSProperties }>
+            <Image
+                className="dark:invert p-2 hover:bg-black/[.04] rounded-full"
+                src="/globe.svg"
+                alt="Logo2"
+                width={70}
+                height={70}
+                priority
+                tabIndex={-1}
+                onClick={(e)=>{e.preventDefault(); changeAbout("placeholder");}}
+            />
+          </div>
+          <div className="moving-element" style={{'--i': 12}as React.CSSProperties }>
+            <Image
+                className="dark:invert p-2 hover:bg-black/[.04] rounded-full"
+                src="/globe.svg"
+                alt="Logo2"
+                width={70}
+                height={70}
+                priority
+                tabIndex={-1}
+                onClick={(e)=>{e.preventDefault(); changeAbout("placeholder");}}
+            />
+          </div>
+          <div className="moving-element" style={{'--i': 13}as React.CSSProperties }>
+            <Image
+                className="dark:invert p-2 hover:bg-black/[.04] rounded-full"
+                src="/globe.svg"
+                alt="Logo2"
+                width={70}
+                height={70}
+                priority
+                tabIndex={-1}
+                onClick={(e)=>{e.preventDefault(); changeAbout("placeholder");}}
+            />
+          </div>
+          <div className="moving-element" style={{'--i': 14}as React.CSSProperties }>
+            <Image
+                className="dark:invert p-2 hover:bg-black/[.04] rounded-full"
+                src="/globe.svg"
+                alt="Logo2"
+                width={70}
+                height={70}
+                priority
+                tabIndex={-1}
+                onClick={(e)=>{e.preventDefault(); changeAbout("placeholder");}}
+            />
+          </div>
+          <div className="moving-element" style={{'--i': 15}as React.CSSProperties }>
+            <Image
+                className="dark:invert p-2 hover:bg-black/[.04] rounded-full"
+                src="/globe.svg"
+                alt="Logo2"
+                width={70}
+                height={70}
+                priority
+                tabIndex={-1}
+                onClick={(e)=>{e.preventDefault(); changeAbout("placeholder");}}
+            />
+          </div>
+          <div className="moving-element" style={{'--i': 16}as React.CSSProperties }>
+            <Image
+                className="dark:invert p-2 hover:bg-black/[.04] rounded-full"
+                src="/globe.svg"
+                alt="Logo2"
+                width={70}
+                height={70}
+                priority
+                tabIndex={-1}
+                onClick={(e)=>{e.preventDefault(); changeAbout("placeholder");}}
+            />
+          </div>
+          <div className="moving-element" style={{'--i': 17}as React.CSSProperties }>
+            <Image
+                className="dark:invert p-2 hover:bg-black/[.04] rounded-full"
+                src="/globe.svg"
+                alt="Logo2"
+                width={70}
+                height={70}
+                priority
+                tabIndex={-1}
+                onClick={(e)=>{e.preventDefault(); changeAbout("placeholder");}}
+            />
+          </div>
+          <div className="moving-element" style={{'--i': 18}as React.CSSProperties }>
+            <Image
+                className="dark:invert p-2 hover:bg-black/[.04] rounded-full"
+                src="/globe.svg"
+                alt="Logo2"
+                width={70}
+                height={70}
+                priority
+                tabIndex={-1}
+                onClick={(e)=>{e.preventDefault(); changeAbout("placeholder");}}
+            />
+          </div>
+          <div className="moving-element" style={{'--i': 19}as React.CSSProperties }>
+            <Image
+                className="dark:invert p-2 hover:bg-black/[.04] rounded-full"
+                src="/globe.svg"
+                alt="Logo2"
+                width={70}
+                height={70}
+                priority
+                tabIndex={-1}
+                onClick={(e)=>{e.preventDefault(); changeAbout("placeholder");}}
+            />
+          </div>
+          <div className="moving-element" style={{'--i': 20}as React.CSSProperties }>
+            <Image
+                className="dark:invert p-2 hover:bg-black/[.04] rounded-full"
+                src="/globe.svg"
+                alt="Logo2"
+                width={70}
+                height={70}
+                priority
+                tabIndex={-1}
+                onClick={(e)=>{e.preventDefault(); changeAbout("placeholder");}}
+            />
+          </div>
+
+
 
         </div>
+
 
 
       </main>
